ce5a0115667dd1132f922873786d87abb88eee28
web-gkumar <kgautam431@gmail.com>
2026-05-10 20:32:45 +0530

    login with google fixing

--- src/app/components/auth/login/login.ts
+++ src/app/components/auth/login/login.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+import { Capacitor } from '@capacitor/core';
 import { HttpClient } from '@angular/common/http';
 import { Auth } from '../../../shared/services/auth';
 
@@ -12,25 +14,50 @@ declare var google: any;
 })
 export class Login implements OnInit, AfterViewInit {
 
-constructor(private http: HttpClient, private authService: Auth) {}
+  user: any = null;
+
+  constructor(private http: HttpClient, private authService: Auth) { }
 
   ngOnInit(): void {
-    
+
   }
 
   ngAfterViewInit() {
-    google.accounts.id.initialize({
-      client_id: '394870904623-c2alhq89rj8r10r5402t5ksk72n440oi.apps.googleusercontent.com',
-      callback: (response: any) => this.authService.handleGoogleResponse(response)
-    });
+    // google.accounts.id.initialize({
+    //   client_id: '394870904623-c2alhq89rj8r10r5402t5ksk72n440oi.apps.googleusercontent.com',
+    //   callback: (response: any) => this.authService.handleGoogleResponse(response)
+    // });
 
-    google.accounts.id.renderButton(
-      document.getElementById('googleBtn'),
-      {
-        theme: 'outline',
-        size: 'large',
-      }
-    );
+    // google.accounts.id.renderButton(
+    //   document.getElementById('googleBtn'),
+    //   {
+    //     theme: 'outline',
+    //     size: 'large',
+    //   }
+    // );
   }
+
+
+
+  async login() {
+    try {
+      if (Capacitor.getPlatform() === 'android') {
+        this.user = await GoogleAuth.signIn();
+      } else {
+        this.user = await GoogleAuth.signIn();
+      }
+      console.log('USER:', this.user);
+    } catch (error) {
+      console.log('LOGIN ERROR', error);
+    }
+  }
+
+  async logout() {
+    await GoogleAuth.signOut();
+    this.user = null;
+  }
+
+
+
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+
+GoogleAuth.initialize({
+  clientId: '394870904623-c2alhq89rj8r10r5402t5ksk72n440oi.apps.googleusercontent.com',
+  scopes: ['profile', 'email'],
+  grantOfflineAccess: true,
+});
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
